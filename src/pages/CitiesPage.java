fcd7be35d2a8426d1be37d8aaf5e8bd7ae4820aa
Dimitrije Stojanovic <stojanovic.dimitrije.mita@gmail.com>
2022-10-10 18:57:58 +0200

    Cities page setup

---
+++ src/pages/CitiesPage.java
@@ -0,0 +1,51 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CitiesPage {
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	public CitiesPage(WebDriver driver) {
+		super();
+		this.driver = driver;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+}
+	public WebElement getNewItemButton() {
+		return driver.findElement(By.className("btnNewItem "));
+	}
+	public WebElement getSearchInput() {
+		return driver.findElement(By.id("search"));
+	}
+	public void waitForEditItemDialogToApper() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dlgNewEditItem ")));
+		
+	}
+	public void waitForDeleteItemDialogToApper() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()=' Cancel ']")));
+		
+	}
+	public WebElement getSaveButton() {
+		return driver.findElement(By.className("btnSave"));
+	}
+	public void waitForTableRowsToApper(int rows) {
+		wait.until(ExpectedConditions.numberOfElementsToBe(By.tagName("tr"), rows + 1));
+	}
+	public WebElement getCellByRow(int cell, int row) {
+		return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td[" + cell + "]"));
+	}
+	public WebElement getEditByRowButton(int row) {
+		return driver.findElement(By.xpath("//tbody/tr[" + row + "]"))
+					 .findElement(By.id("edit"));
+	}
+	public WebElement getDeleteByRowButton(int row) {
+		return driver.findElement(By.xpath("//tbody/tr[" + row + "]"))
+					 .findElement(By.id("delete"));
+	}
+}
--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -15,6 +15,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import pages.CitiesPage;
 import pages.LoginPage;
 import pages.NavPage;
 import pages.SingUpPage;
@@ -25,17 +26,18 @@ public class BasicTest {
 	protected LoginPage loginPage;
 	protected NavPage navPage;
 	protected SingUpPage singUpPage;
+	protected CitiesPage citiesPage;
 
 	@BeforeClass
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
 		loginPage = new LoginPage(driver);
 		navPage = new NavPage(driver);
 		singUpPage = new SingUpPage(driver);
-		
+		citiesPage = new CitiesPage(driver);
 	}
 
 	@BeforeMethod
